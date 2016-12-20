ded623013f95db5ba5739fd136b40ea0ef8bcf28
Lukasz Guminski <lguminski@gmail.com>
2016-12-20 17:22:00 +0100

    adding comments

--- scripts/0-settings.sh
+++ scripts/0-settings.sh
@@ -1,6 +1,8 @@
 # this project create 3 clusters in 3 zones. FED_ZONE points to the one, which will be used to deploy feration plane
 export FED_ZONE=us-east1-b
+
 # Google Cloud project name
-export FED_PROJECT=container-solutions
+export FED_PROJECT=<YOUR PROJECT e.g. company-project>
+
 # DNS suffix for this federation. Federated Service DNS names are published with this suffix.
-export FED_DNS_ZONE=infra.container-solutions.com
+export FED_DNS_ZONE=<YOUR DNS SUFFIX e.g. example.com>
